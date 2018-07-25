068af7dbe63650a1c9e66de5c254f1dab46ac818
rush <rush@rushs-MacBook-Pro.local>
2018-07-24 17:22:17 -0700

    Implemented dynamically changing roi; still needs some work

--- Real_Car/Extract_Frames_From_Video.py
+++ Real_Car/Extract_Frames_From_Video.py
@@ -3,15 +3,16 @@ This program extracts frames from videos
 """
 import cv2
 
-path = 'project_video_output_with_vehicle_detection.mp4' # path to the input video you wish to extract frames frame
+path = 'Real_Car_Images_And_Videos/Bay_Area_Videos/Highway_280_3-31-2018.mp4' # path to the input video you wish to extract frames frame
 vidcap = cv2.VideoCapture(path)
 success,image = vidcap.read()
 
 count = 0
 success = True
 
-while success:
+while (success and count <= 2179):
   success,image = vidcap.read()
-  print('Read a new frame: ', success)
-  cv2.imwrite('frames/%d.jpg' % count, image) # path you wish to save each of your frames to; save each frame as JPEG file
+  print('Read a new frame: ', count)
+  if (count >= 1703):
+      cv2.imwrite('Real_Car_Images_And_Videos/Bay_Area_Videos/subclip3_frames/%d.jpg' % (count-1703), image) # path you wish to save each of your frames to; save each frame as JPEG file
   count += 1
